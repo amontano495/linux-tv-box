a9e482235523e83111e39ce70ea698b9d05e2ea1
Adam Montano <amontano495@gmail.com>
2024-10-29 18:44:19 -0700

    updated readme and install script

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Turn a PC into a simple and approachable Smart TV replacement with support for things beyond Kodi
 
 # Install
-On a basic Debian install with no display server (but with SSH installed)
+On a basic Debian install with no display server (but with SSH installed) and with a user (named "user") created.
 ```
-$ xargs sudo apt -y install < packages
+$ chmod + x
+$ sudo install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 xargs sudo apt -y install < packages
-mkdir -p ~/.config/autostart
-sudo mv autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
 wget https://github.com/complexlogic/flex-launcher/releases/download/v2.1/flex-launcher_2.1_amd64.deb
 sudo apt install ./flex-launcher_2.1_amd64.deb
+cp -r .config $HOME
+mkdir -p $HOME/.config/autostart
 cp /usr/share/applications/flex-launcher.desktop ~/.config/autostart
+sudo cp autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
